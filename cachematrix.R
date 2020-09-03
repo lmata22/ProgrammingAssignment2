2f1baa5bd9ed8be4c809a85cfda15455179aa6e8
lmata22 <52493440+lmata22@users.noreply.github.com>
2020-09-04 00:38:47 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## la funcion halla la matriz inversa a la dada
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mat <- NULL
+  set <<- function(y){
+    x <<- y
+    mat <<- NULL
+  }
+  get <- function() x
+  inv1 <- function(sol) mat <<- sol
+  inv2 <- function() mat
+  list(set=set, get=get, inv1=inv1, inv2=inv2)
 }
 
 
-## Write a short comment describing this function
+## la funcion guarda la matriz en la cache para no calcularla cada vez que se use
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mat <- x$getinverse()
+  if(!is.null(mat)){
+    return(mat)
+  }
+  data <- x$get()
+  mat <- solve(data, ...)
+  x$setinverse(mat)
+  mat
 }
